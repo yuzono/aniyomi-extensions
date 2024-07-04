2da8cae3e7a880de7dfe317535eb50283e8df528
Dark25 <nadiecaca2000@gmail.com>
2024-07-04 22:36:15 +0200

    fix ANIMEWORLD

--- src/it/animeworld/src/eu/kanade/tachiyomi/animeextension/it/animeworld/ANIMEWORLD.kt
+++ src/it/animeworld/src/eu/kanade/tachiyomi/animeextension/it/animeworld/ANIMEWORLD.kt
@@ -17,7 +17,6 @@ import eu.kanade.tachiyomi.lib.filemoonextractor.FilemoonExtractor
 import eu.kanade.tachiyomi.lib.streamtapeextractor.StreamTapeExtractor
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
-import eu.kanade.tachiyomi.util.parallelCatchingFlatMapBlocking
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import okhttp3.HttpUrl.Companion.toHttpUrl
@@ -140,7 +139,7 @@ class ANIMEWORLD : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
             serverList.add(Pair(name, url))
         }
 
-        val videoList = serverList.parallelCatchingFlatMapBlocking { server ->
+        val videoList = serverList.flatMap { server ->
             val url = server.second
             when {
                 url.contains("streamingaw") -> {
