keiyoushi/extensions-source

[ci] build only required modules (#6900)
* workflow

* configs

* workflow

* workflow

* workflow

* workflow

* workflow

* workflow

* workflow

* workflow

* workflow

* workflow

* rewrite python scripts

* small tweak

* Update .github/scripts/modules-to-build.py

Co-authored-by: AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>

* set full path of git

* now?

* i hate my life

* and should work now

* And some more changes

* And some more changes 2

* Oops

* support for always building some modules

* string replace fix

* don't run matrix chunk if empty

* bruh moment

* revert

* test pr workflow

- remove extension
- modify extension
- modify multisrc

* fix gradlew command

* Revert "test pr workflow"

This reverts commit f14bb233863334396335e123f2dfac617cca9d23.

* skip step

* skip step x2

* perhaps

* make if work?

* Reapply "test pr workflow"

This reverts commit d92bf2c7532ec7bdf7a5ce8e1e23547cbefe224a.

* make if work!

* Revert "Reapply "test pr workflow""

This reverts commit 5122ebd164348f6c140a12e20dfcfb3c72212c0c.

* update job name

* Revert "update job name"

This reverts commit 3fd40c40f4ff625b1948b1051d5e951a861b9704.

* final changes

* Cleanup some common code

* Cleanup settings.gradle.kts

* Reapply "Reapply "test pr workflow""

This reverts commit df3ade2e07b60615077f05a35bd2344d0fdbcea4.

* Revert "Reapply "Reapply "test pr workflow"""

This reverts commit 028db57212ec5516442f227ba8aee507e9183f8d.

* correct delete output name

* pr test: multiple matrices

* Revert "pr test: multiple matrices"

This reverts commit 399e33e16abf2011ae6bc7f0113eb9ee641fc0a9.

---------

Co-authored-by: FourTOne5 <107297513+FourTOne5@users.noreply.github.com>

.github/scripts/merge-repo.py
@@ -1,0 +1,48 @@
+import html
+import sys
+import json
+from pathlib import Path
+import shutil
+
+REMOTE_REPO: Path = Path.cwd()
+LOCAL_REPO: Path = REMOTE_REPO.parent.joinpath("main/repo")
+
+to_delete: list[str] = json.loads(sys.argv[1])
+
+for module in to_delete:
+    apk_name = f"tachiyomi-{module}-v*.*.*.apk"
+    icon_name = f"eu.kanade.tachiyomi.extension.{module}.png"
+    for file in REMOTE_REPO.joinpath("apk").glob(apk_name):
+        file.unlink()
+    for file in REMOTE_REPO.joinpath("icon").glob(icon_name):
+        file.unlink()
+
+shutil.copytree(src=LOCAL_REPO.joinpath("apk"), dst=REMOTE_REPO.joinpath("apk"))
+shutil.copytree(src=LOCAL_REPO.joinpath("icon"), dst=REMOTE_REPO.joinpath("icon"))
+
+with REMOTE_REPO.joinpath("index.min.json").open() as remote_index_file:
+    remote_index = json.load(remote_index_file)
+
+with LOCAL_REPO.joinpath("index.min.json").open() as local_index_file:
+    local_index = json.load(local_index_file)
+
+index = [
+    item for item in remote_index
+    if not any([item["pkg"].endswith(f".{module}") for module in to_delete])
+]
+index.extend(local_index)
+index.sort(key=lambda x: x["pkg"])
+
+with REMOTE_REPO.joinpath("index.json").open("w", encoding="utf-8") as index_file:
+    json.dump(index, index_file, ensure_ascii=False, indent=2)
+
+with REMOTE_REPO.joinpath("index.min.json").open("w", encoding="utf-8") as index_min_file:
+    json.dump(index, index_min_file, ensure_ascii=False, separators=(",", ":"))
+
+with REMOTE_REPO.joinpath("index.html").open("w", encoding="utf-8") as index_html_file:
+    index_html_file.write('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>apks</title>\n</head>\n<body>\n<pre>\n')
+    for entry in index:
+        apk_escaped = 'apk/' + html.escape(entry["apk"])
+        name_escaped = html.escape(entry["name"])
+        index_html_file.write(f'<a href="{apk_escaped}">{name_escaped}</a>\n')
+    index_html_file.write('</pre>\n</body>\n</html>\n')

yuzono/aniyomi-extensions

This reverts commit 8c3aa34555a26f2257d2d791104eb63bbd48856c.
This reverts commit 9c7f2a7acd787b18b98ef99da733cc2f8077b2a7.
This reverts commit 43238b0d7bfaa81b8067c7206bb9fb081fa4dbba.
This reverts commit 361b6e7279b901a78fac3eb77e909f480cf54475.
This reverts commit 68524b649974e4d05b6a01d0ec297a5f28617071.
This reverts commit bafab8b8405cff9b9142d9f83c4f5adac92da6a0.
This reverts commit 87fd5fcfd01dc4d83658312293fcc8ac3586086e.
(cherry picked from commit 14ef26a5a6f417aa6f003a338f1ae79830ec49a2)

.github/scripts/merge-repo.py
@@ -1,0 +1,48 @@
+import html
+import sys
+import json
+from pathlib import Path
+import shutil
+
+REMOTE_REPO: Path = Path.cwd()
+LOCAL_REPO: Path = REMOTE_REPO.parent.joinpath("main/repo")
+
+to_delete: list[str] = json.loads(sys.argv[1])
+
+for module in to_delete:
+    apk_name = f"aniyomi-{module}-v*.*.*.apk"
+    icon_name = f"eu.kanade.tachiyomi.animeextension.{module}.png"
+    for file in REMOTE_REPO.joinpath("apk").glob(apk_name):
+        file.unlink()
+    for file in REMOTE_REPO.joinpath("icon").glob(icon_name):
+        file.unlink()
+
+shutil.copytree(src=LOCAL_REPO.joinpath("apk"), dst=REMOTE_REPO.joinpath("apk"))
+shutil.copytree(src=LOCAL_REPO.joinpath("icon"), dst=REMOTE_REPO.joinpath("icon"))
+
+with REMOTE_REPO.joinpath("index.min.json").open() as remote_index_file:
+    remote_index = json.load(remote_index_file)
+
+with LOCAL_REPO.joinpath("index.min.json").open() as local_index_file:
+    local_index = json.load(local_index_file)
+
+index = [
+    item for item in remote_index
+    if not any([item["pkg"].endswith(f".{module}") for module in to_delete])
+]
+index.extend(local_index)
+index.sort(key=lambda x: x["pkg"])
+
+with REMOTE_REPO.joinpath("index.json").open("w", encoding="utf-8") as index_file:
+    json.dump(index, index_file, ensure_ascii=False, indent=2)
+
+with REMOTE_REPO.joinpath("index.min.json").open("w", encoding="utf-8") as index_min_file:
+    json.dump(index, index_min_file, ensure_ascii=False, separators=(",", ":"))
+
+with REMOTE_REPO.joinpath("index.html").open("w", encoding="utf-8") as index_html_file:
+    index_html_file.write('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>apks</title>\n</head>\n<body>\n<pre>\n')
+    for entry in index:
+        apk_escaped = 'apk/' + html.escape(entry["apk"])
+        name_escaped = html.escape(entry["name"])
+        index_html_file.write(f'<a href="{apk_escaped}">{name_escaped}</a>\n')
+    index_html_file.write('</pre>\n</body>\n</html>\n')