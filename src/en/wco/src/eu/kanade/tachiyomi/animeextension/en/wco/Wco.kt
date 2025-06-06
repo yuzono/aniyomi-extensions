f4fa9aab466b3feae7719672fd1f625664104718
Cuong-Tran <cuongtran.tm@gmail.com>
2025-06-06 16:57:20 +0700

    Fix WCO's empty popular anime

--- src/en/wco/src/eu/kanade/tachiyomi/animeextension/en/wco/Wco.kt
+++ src/en/wco/src/eu/kanade/tachiyomi/animeextension/en/wco/Wco.kt
@@ -1,10 +1,20 @@
 package eu.kanade.tachiyomi.animeextension.en.wco
 
+import eu.kanade.tachiyomi.animesource.model.AnimesPage
 import eu.kanade.tachiyomi.multisrc.wcotheme.WcoTheme
+import eu.kanade.tachiyomi.util.asJsoup
+import okhttp3.Response
 
 class Wco : WcoTheme() {
     override val name = "Watch Cartoon Online"
     override val baseUrl = "https://www.wco.tv"
 
     override val useOldIframeExtractor = true
+
+    override fun popularAnimeParse(response: Response): AnimesPage {
+        val document = response.asJsoup()
+        return document.select(popularAnimeSelector()).map { popularAnimeFromElement(it) }
+            .filterNot { it.url == "/" }
+            .let { AnimesPage(it, false) }
+    }
 }
