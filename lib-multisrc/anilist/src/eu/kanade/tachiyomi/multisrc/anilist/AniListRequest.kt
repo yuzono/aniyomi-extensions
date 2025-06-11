4c1940e16fdf35509be9ff79cf5bc5215e2b20d9
Cuong-Tran <cuongtran.tm@gmail.com>
2025-06-11 23:13:58 +0700

    (multi-src/Anilist): Normalize query from $ to %

--- lib-multisrc/anilist/src/eu/kanade/tachiyomi/multisrc/anilist/AniListRequest.kt
+++ lib-multisrc/anilist/src/eu/kanade/tachiyomi/multisrc/anilist/AniListRequest.kt
@@ -2,112 +2,138 @@ package eu.kanade.tachiyomi.multisrc.anilist
 
 import kotlinx.serialization.Serializable
 
+private fun String.toQuery() = this.trimIndent().replace("%", "$")
+
 internal const val MEDIA_QUERY = """
     id
     title {
-      romaji
-      english
-      native
+        romaji
+        english
+        native
     }
     coverImage {
-      large
+        large
     }
     description
     status
     genres
     studios(isMain: true) {
-      nodes {
-        name
-      }
+        nodes {
+            name
+        }
     }
 """
 
-internal const val ANIME_LIST_QUERY = """
-    query (${"$"}page: Int, ${"$"}sort: [MediaSort], ${"$"}search: String) {
-      Page(page: ${"$"}page, perPage: 30) {
+internal val ANIME_LIST_QUERY = """
+query (
+    %page: Int,
+    %sort: [MediaSort],
+    %search: String,
+) {
+    Page(page: %page, perPage: 30) {
         pageInfo {
-          hasNextPage
+            hasNextPage
         }
-        media(type: ANIME, sort: ${"$"}sort, search: ${"$"}search, status_in: [RELEASING, FINISHED], countryOfOrigin: "JP", isAdult: false) {
-          $MEDIA_QUERY
+        media(
+            type: ANIME,
+            sort: %sort,
+            search: %search,
+            status_in: [RELEASING, FINISHED],
+            countryOfOrigin: "JP",
+            isAdult: false,
+        ) {
+            $MEDIA_QUERY
         }
-      }
     }
-"""
+}
+""".toQuery()
 
-internal const val LATEST_ANIME_LIST_QUERY = """
-    query (${"$"}page: Int, ${"$"}sort: [MediaSort], ${"$"}search: String) {
-      Page(page: ${"$"}page, perPage: 30) {
+internal val LATEST_ANIME_LIST_QUERY = """
+query (
+    %page: Int,
+    %sort: [MediaSort],
+    %search: String,
+) {
+    Page(page: %page, perPage: 30) {
         pageInfo {
-          hasNextPage
+            hasNextPage
         }
-        media(type: ANIME, sort: ${"$"}sort, search: ${"$"}search, status_in: [RELEASING, FINISHED], countryOfOrigin: "JP", isAdult: false, startDate_greater: 1, episodes_greater: 1) {
-          $MEDIA_QUERY
+        media(
+            type: ANIME,
+            sort: %sort,
+            search: %search,
+            status_in: [RELEASING, FINISHED],
+            countryOfOrigin: "JP",
+            isAdult: false,
+            startDate_greater: 1,
+            episodes_greater: 1,
+        ) {
+            $MEDIA_QUERY
         }
-      }
     }
-"""
+}
+""".toQuery()
 
-internal const val ANIME_DETAILS_QUERY = """
-    query (${"$"}id: Int) {
-      Media(id: ${"$"}id) {
+internal val ANIME_DETAILS_QUERY = """
+query (%id: Int) {
+    Media(id: %id) {
         $MEDIA_QUERY
-      }
     }
-"""
-private fun String.toQuery() = this.trimIndent().replace("%", "$")
+}
+""".toQuery()
+
 internal val SORT_QUERY = """
-    query (
-        ${"$"}page: Int,
-        ${"$"}perPage: Int,
-        ${"$"}isAdult: Boolean,
-        ${"$"}type: MediaType,
-        ${"$"}sort: [MediaSort],
-        ${"$"}status: MediaStatus,
-        ${"$"}search: String,
-        ${"$"}genres: [String],
-        ${"$"}year: String,
-        ${"$"}seasonYear: Int,
-        ${"$"}season: MediaSeason,
-        ${"$"}format: [MediaFormat]
-    ) {
-        Page (page: ${"$"}page, perPage: ${"$"}perPage) {
-            pageInfo {
-                hasNextPage
+query (
+    %page: Int,
+    %perPage: Int,
+    %isAdult: Boolean,
+    %type: MediaType,
+    %sort: [MediaSort],
+    %status: MediaStatus,
+    %search: String,
+    %genres: [String],
+    %year: String,
+    %seasonYear: Int,
+    %season: MediaSeason,
+    %format: [MediaFormat]
+) {
+    Page (page: %page, perPage: %perPage) {
+        pageInfo {
+            hasNextPage
+        }
+        media (
+            isAdult: %isAdult,
+            type: %type,
+            sort: %sort,
+            status: %status,
+            search: %search,
+            genre_in: %genres,
+            startDate_like: %year,
+            seasonYear: %seasonYear,
+            season: %season,
+            format_in: %format
+        ) {
+            id
+            title {
+                romaji
+                english
+                native
             }
-            media (
-                isAdult: ${"$"}isAdult,
-                type: ${"$"}type,
-                sort: ${"$"}sort,
-                status: ${"$"}status,
-                search: ${"$"}search,
-                genre_in: ${"$"}genres,
-                startDate_like: ${"$"}year,
-                seasonYear: ${"$"}seasonYear,
-                season: ${"$"}season,
-                format_in: ${"$"}format
-            ) {
-                id
-                title {
-                    romaji
-                    english
-                    native
-                }
-                coverImage {
-                    extraLarge
-                    large
-                    medium
-                }
-                status
-                genres
-                studios {
-                    nodes {
-                        name
-                    }
+            coverImage {
+                extraLarge
+                large
+                medium
+            }
+            status
+            genres
+            studios {
+                nodes {
+                    name
                 }
             }
         }
     }
+}
 """.toQuery()
 
 @Serializable
