ba9c65573f5771aa31db2161697a8452eb189b50
Secozzi <49240133+secozzi@users.noreply.github.com>
2025-07-23 15:47:21 +0200

    Some fixes (Secozzi/aniyomi-extensions#22)
    * Move common stuff to `utils` module
    
    * Move more stuff to utils & make stremio not require restart if possible
    
    * Bump versions & small cleanup in build files
    
    * Use context parameters for json utils
    
    * Add spotless
    
    * Simplify build process
    
    * bump versions
    
    (cherry picked from commit cf53bcf647296aaaa793acd2e1aac0a486418665)
    
    ---------
    
    Co-authored-by: Cuong-Tran <cuongtran.tm@gmail.com>

--- src/all/jellyfin/src/eu/kanade/tachiyomi/animeextension/all/jellyfin/dto/ItemDto.kt
+++ src/all/jellyfin/src/eu/kanade/tachiyomi/animeextension/all/jellyfin/dto/ItemDto.kt
@@ -215,13 +215,13 @@ data class ItemDto(
 }
 
 @Serializable
-class SessionDto(
+data class SessionDto(
     val mediaSources: List<MediaDto>,
     val playSessionId: String,
 )
 
 @Serializable
-class MediaDto(
+data class MediaDto(
     val size: Long? = null,
     val id: String? = null,
     val bitrate: Long? = null,
@@ -231,7 +231,7 @@ class MediaDto(
     val mediaStreams: List<MediaStreamDto>,
 ) {
     @Serializable
-    class MediaStreamDto(
+    data class MediaStreamDto(
         val codec: String,
         val index: Int,
         val type: String,
