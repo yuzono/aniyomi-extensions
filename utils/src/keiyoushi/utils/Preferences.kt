keiyoushi/extensions-source

add helper utils (#7683)
* add some helper utilities

* adjust parameter

utils/src/keiyoushi/utils/Preferences.kt
@@ -1,0 +1,29 @@
+package keiyoushi.utils
+
+import android.app.Application
+import android.content.SharedPreferences
+import eu.kanade.tachiyomi.source.online.HttpSource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+
+/**
+ * Returns the [SharedPreferences] associated with current source id
+ */
+inline fun HttpSource.getPreferences(
+    migration: SharedPreferences.() -> Unit = { },
+): SharedPreferences = getPreferences(id).also(migration)
+
+/**
+ * Lazily returns the [SharedPreferences] associated with current source id
+ */
+inline fun HttpSource.getPreferencesLazy(
+    crossinline migration: SharedPreferences.() -> Unit = { }
+) = lazy { getPreferences(migration) }
+
+/**
+ * Returns the [SharedPreferences] associated with passed source id
+ */
+@Suppress("NOTHING_TO_INLINE")
+inline fun getPreferences(sourceId: Long): SharedPreferences =
+    Injekt.get<Application>().getSharedPreferences("source_$sourceId", 0x0000)
+

yuzono/aniyomi-extensions

(cherry picked from commit 1b5d54b6f089625e5567993185ac33b8989843ca)

utils/src/keiyoushi/utils/Preferences.kt
@@ -1,0 +1,28 @@
+package keiyoushi.utils
+
+import android.app.Application
+import android.content.SharedPreferences
+import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
+
+/**
+ * Returns the [SharedPreferences] associated with current source id
+ */
+inline fun AnimeHttpSource.getPreferences(
+    migration: SharedPreferences.() -> Unit = { },
+): SharedPreferences = getPreferences(id).also(migration)
+
+/**
+ * Lazily returns the [SharedPreferences] associated with current source id
+ */
+inline fun AnimeHttpSource.getPreferencesLazy(
+    crossinline migration: SharedPreferences.() -> Unit = { }
+) = lazy { getPreferences(migration) }
+
+/**
+ * Returns the [SharedPreferences] associated with passed source id
+ */
+@Suppress("NOTHING_TO_INLINE")
+inline fun getPreferences(sourceId: Long): SharedPreferences =
+    Injekt.get<Application>().getSharedPreferences("source_$sourceId", 0x0000)