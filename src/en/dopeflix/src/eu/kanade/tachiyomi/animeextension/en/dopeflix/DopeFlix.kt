616077cb37422a09c937a3b4a438ea0515ad4bb3
Cuong-Tran <cuongtran.tm@gmail.com>
2025-10-07 12:03:03 +0700

    feat(dopeflix): Re-done DopeBox/SFlix & more domains (#402)
    * initial code from zoro-theme
    
    * fix browsing
    
    * filter list
    
    * fix getDetails
    
    * extract videos
    
    * better sorting
    
    * Add cover
    
    * Filters
    
    * Migrate to DopeFlix multi-src
    
    * cacheControl
    
    * sort by quality
    
    * preferred server for DopeBox
    
    * reorder Filter
    
    * Correct headers
    
    * Cleanup
    
    * make open
    
    * Make url compatible between sites
    
    * more domain & lazy
    
    * Fix year filters
    
    * use built-in function
    
    * avoid null evil
    
    * refactor
    
    * improve
    
    * cache control 1 hour
    
    * update domain list
    
    * add preferences for popular type and latest priority; refactor selectors for anime retrieval
    
    * rename CitySonic to DopeFlix
    
    * fix: clear old hosts & domain
    
    * bump version
    
    * fix: update preference clearing method to remove old preferences
    
    ---------
    
    Co-authored-by: krysanify <>

---
+++ src/en/dopeflix/src/eu/kanade/tachiyomi/animeextension/en/dopeflix/DopeFlix.kt
@@ -0,0 +1,23 @@
+package eu.kanade.tachiyomi.animeextension.en.dopeflix
+
+import eu.kanade.tachiyomi.animeextension.BuildConfig
+import eu.kanade.tachiyomi.multisrc.dopeflix.DopeFlix
+
+class DopeFlix : DopeFlix(
+    "DopeFlix",
+    "en",
+    BuildConfig.MEGACLOUD_API,
+    listOf(
+        "gomovies.gg",
+        "himovies.sx",
+        "fmovieszz.lol",
+        "movies4kto.lol",
+        "series2watch.net",
+        // "citysonic.tv",
+    ),
+) {
+    override val detailInfoSelector by lazy { "div.detail_page-infor, div.m_i-detail" }
+    override val coverSelector by lazy { "div.cover_follow, div.dp-w-cover, div.w_b-cover" }
+
+    override val episodeRegex by lazy { """Eps (\d+)""".toRegex() }
+}
