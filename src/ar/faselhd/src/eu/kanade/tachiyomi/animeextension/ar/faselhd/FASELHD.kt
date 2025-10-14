943f5938a4bec2d9766aa4080d6f5c8051a9af52
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-10-14 08:57:13 +0700

    feat(FaselHD): add custom domain preference and update domain

--- src/ar/faselhd/src/eu/kanade/tachiyomi/animeextension/ar/faselhd/FASELHD.kt
+++ src/ar/faselhd/src/eu/kanade/tachiyomi/animeextension/ar/faselhd/FASELHD.kt
@@ -1,5 +1,7 @@
 package eu.kanade.tachiyomi.animeextension.ar.faselhd
 
+import android.content.SharedPreferences
+import android.text.InputType
 import androidx.preference.PreferenceScreen
 import eu.kanade.tachiyomi.animesource.ConfigurableAnimeSource
 import eu.kanade.tachiyomi.animesource.model.AnimeFilter
@@ -13,7 +15,9 @@ import eu.kanade.tachiyomi.lib.synchrony.Deobfuscator
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.util.asJsoup
 import eu.kanade.tachiyomi.util.parallelCatchingFlatMapBlocking
+import extensions.utils.addEditTextPreference
 import extensions.utils.addListPreference
+import extensions.utils.delegate
 import extensions.utils.getPreferencesLazy
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
@@ -26,13 +30,14 @@ class FASELHD : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     override val name = "فاصل اعلاني"
 
-    override val baseUrl = "https://www.faselhds.life"
+    private val preferences by getPreferencesLazy()
+
+    override val baseUrl
+        get() = preferences.customDomain.ifBlank { "https://www.faselhds.biz" }
 
     override val lang = "ar"
 
     override val supportsLatest = true
-
-    private val preferences by getPreferencesLazy()
 
     private val playlistUtils by lazy { PlaylistUtils(client, headers) }
 
@@ -123,7 +128,7 @@ class FASELHD : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     }
 
     override fun List<Video>.sort(): List<Video> {
-        val quality = preferences.getString("preferred_quality", "1080")!!
+        val quality = preferences.quality
         return sortedWith(
             compareBy { it.quality.contains(quality) },
         ).reversed()
@@ -270,15 +275,35 @@ class FASELHD : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
     }
 
     // preferred quality settings
+    private var SharedPreferences.customDomain by preferences.delegate(PREF_DOMAIN_CUSTOM_KEY, "")
+    private var SharedPreferences.quality by preferences.delegate(PREF_QUALITY_KEY, PREF_QUALITY_DEFAULT)
 
     override fun setupPreferenceScreen(screen: PreferenceScreen) {
         screen.addListPreference(
-            key = "preferred_quality",
-            title = "Preferred quality",
+            key = PREF_QUALITY_KEY,
+            title = "الجودة المفضلة",
             entries = listOf("1080p", "720p", "480p", "360p"),
             entryValues = listOf("1080", "720", "480", "360"),
-            default = "1080",
+            default = PREF_QUALITY_DEFAULT,
             summary = "%s",
         )
+
+        screen.addEditTextPreference(
+            key = PREF_DOMAIN_CUSTOM_KEY,
+            default = "",
+            title = "المجال المخصص",
+            dialogMessage = "أدخل المجال المخصص (على سبيل المثال، https://example.com)",
+            summary = preferences.customDomain,
+            getSummary = { it },
+            inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_URI,
+            validate = { it.isBlank() || (it.toHttpUrlOrNull() != null && !it.endsWith("/")) },
+            validationMessage = { "عنوان URL غير صالح أو مشوه أو ينتهي بشرطة مائلة" },
+        )
+    }
+
+    companion object {
+        private const val PREF_DOMAIN_CUSTOM_KEY = "custom_domain"
+        private const val PREF_QUALITY_KEY = "preferred_quality"
+        private const val PREF_QUALITY_DEFAULT = "1080"
     }
 }
