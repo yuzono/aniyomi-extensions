16e65fbee881880c3ab70aa5f642c23af03a603a
Yogesh S <yogesh.sakthivel@thoughtscrest.com>
2025-10-25 14:33:20 +0530

    Fixed Extractors

--- src/en/donghuastream/src/eu/kanade/tachiyomi/animeextension/en/donghuastream/DonghuaStream.kt
+++ src/en/donghuastream/src/eu/kanade/tachiyomi/animeextension/en/donghuastream/DonghuaStream.kt
@@ -1,12 +1,21 @@
 package eu.kanade.tachiyomi.animeextension.en.donghuastream
 
+import android.content.SharedPreferences
+import androidx.preference.PreferenceScreen
+import eu.kanade.tachiyomi.animeextension.en.donghuastream.extractors.RumbleExtractor
 import eu.kanade.tachiyomi.animeextension.en.donghuastream.extractors.StreamPlayExtractor
 import eu.kanade.tachiyomi.animesource.model.AnimeFilterList
+import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.dailymotionextractor.DailymotionExtractor
+import eu.kanade.tachiyomi.lib.okruextractor.OkruExtractor
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
 import eu.kanade.tachiyomi.network.GET
+import extensions.utils.LazyMutable
+import extensions.utils.addSetPreference
+import extensions.utils.addSwitchPreference
 import okhttp3.Request
+import okhttp3.Response
 
 class DonghuaStream : AnimeStream(
     "en",
@@ -24,16 +33,69 @@ class DonghuaStream : AnimeStream(
 
     override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request = GET("$baseUrl/pagg/$page/?s=$query")
 
+    private var SharedPreferences.ignorePreview
+        by LazyMutable { preferences.getBoolean(IGNORE_PREVIEW_KEY, IGNORE_PREVIEW_DEFAULT) }
+
+    private var SharedPreferences.getHosters
+        by LazyMutable { preferences.getStringSet(PREF_HOSTER_KEY, PREF_HOSTER_DEFAULT)!! }
+
+    private companion object {
+        private const val PREF_HOSTER_KEY = "dm_hoster_selection"
+        private val INTERNAL_HOSTER_NAMES = listOf("Dailymotion", "Streamplay", "Rumble", "Ok ru")
+        private val PREF_HOSTER_ENTRY_VALUES = INTERNAL_HOSTER_NAMES.map { it.lowercase() }.toList()
+        private val PREF_HOSTER_DEFAULT = setOf("dailymotion,streamplay,rumble,ok ru")
+
+        private const val IGNORE_PREVIEW_KEY = "dm_ignore_preview"
+        private const val IGNORE_PREVIEW_DEFAULT = true
+    }
+
+    override fun setupPreferenceScreen(screen: PreferenceScreen) {
+        super.setupPreferenceScreen(screen)
+
+        screen.addSetPreference(
+            key = PREF_HOSTER_KEY,
+            title = "Enable/Disable Hosts",
+            summary = "",
+            entries = INTERNAL_HOSTER_NAMES,
+            entryValues = PREF_HOSTER_ENTRY_VALUES,
+            default = PREF_HOSTER_DEFAULT,
+        ) {
+            preferences.getHosters = it
+        }
+
+        screen.addSwitchPreference(
+            key = IGNORE_PREVIEW_KEY,
+            title = "Skip Preview episodes",
+            summary = "",
+            default = IGNORE_PREVIEW_DEFAULT,
+        ) {
+            preferences.ignorePreview = it
+        }
+    }
+
+    override val prefQualityValues = arrayOf("2160p", "1440p", "1080p", "720p", "480p", "360p")
+    override val prefQualityEntries = prefQualityValues
+
+    override fun episodeListParse(response: Response): List<SEpisode> {
+        return super.episodeListParse(response)
+            .filter { !it.name.contains("Preview") || !preferences.ignorePreview }
+    }
+
     // ============================ Video Links =============================
 
     private val dailymotionExtractor by lazy { DailymotionExtractor(client, headers) }
     private val streamPlayExtractor by lazy { StreamPlayExtractor(client, headers) }
+    private val okruExtractor by lazy { OkruExtractor(client) }
+
+    private val rumbleExtractor by lazy { RumbleExtractor(client, headers) }
 
     override fun getVideoList(url: String, name: String): List<Video> {
         val prefix = "$name - "
         return when {
-            url.contains("dailymotion") -> dailymotionExtractor.videosFromUrl(url, prefix = prefix)
-            url.contains("streamplay") -> streamPlayExtractor.videosFromUrl(url, prefix = prefix)
+            preferences.getHosters.contains("dailymotion") and url.contains("dailymotion") -> dailymotionExtractor.videosFromUrl(url, prefix = prefix)
+            preferences.getHosters.contains("streamplay") and url.contains("streamplay") -> streamPlayExtractor.videosFromUrl(url, prefix = prefix)
+            preferences.getHosters.contains("ok ru") and url.contains("ok.ru") -> okruExtractor.videosFromUrl("https:$url", prefix = prefix)
+            preferences.getHosters.contains("rumble") and url.contains("rumble") -> rumbleExtractor.videosFromUrl("https:$url", prefix = prefix)
             else -> emptyList()
         }
     }
---
+++ src/en/donghuastream/src/eu/kanade/tachiyomi/animeextension/en/donghuastream/extractors/RumbleExtractor.kt
@@ -0,0 +1,21 @@
+package eu.kanade.tachiyomi.animeextension.en.donghuastream.extractors
+
+import eu.kanade.tachiyomi.animesource.model.Video
+import eu.kanade.tachiyomi.lib.playlistutils.PlaylistUtils
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+
+class RumbleExtractor(private val client: OkHttpClient, private val headers: Headers) {
+
+    private val playlistUtils by lazy { PlaylistUtils(client, headers) }
+
+    fun videosFromUrl(url: String, prefix: String = ""): List<Video> {
+        val sourceUrl = "https://rumble.com/hls-vod/${extractRumbleId(url)}/playlist.m3u8"
+        return playlistUtils.extractFromHls(sourceUrl, referer = url, subtitleList = emptyList(), videoNameGen = { q -> "$prefix $q" })
+    }
+
+    fun extractRumbleId(url: String): String? {
+        val regex = Regex("""rumble\.com/embed/v([a-zA-Z0-9]+)""")
+        return regex.find(url)?.groupValues?.get(1)
+    }
+}
