46a7242eed7bf774a604a3d9ad3f5c6167f3ab2a
smash955 <60195761+smash955@users.noreply.github.com>
2025-12-24 18:53:04 +0100

    Update AnimeSamaUrlActivity.kt

--- src/fr/animesama/src/eu/kanade/tachiyomi/animeextension/fr/animesama/AnimeSamaUrlActivity.kt
+++ src/fr/animesama/src/eu/kanade/tachiyomi/animeextension/fr/animesama/AnimeSamaUrlActivity.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import kotlin.system.exitProcess
 
 /**
- * Springboard that accepts https://anime-sama.si/anime/<item> intents
+ * Springboard that accepts https://anime-sama.si/catalogue/<item> intents
  * and redirects them to the main Aniyomi process.
  */
 class AnimeSamaUrlActivity : Activity() {
