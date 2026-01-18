3cf89679c2aaf83965ee3b2cd66dece5a0b67314
hakim12sidih-netizen <hakim12sidih@gmail.com>
2026-01-18 07:41:21 +0100

    Fix: Cloudflare headers and compilation

--- src/fr/franime/src/eu/kanade/tachiyomi/animeextension/fr/franime/FrAnime.kt
+++ src/fr/franime/src/eu/kanade/tachiyomi/animeextension/fr/franime/FrAnime.kt
@@ -40,7 +40,7 @@ class FrAnime : AnimeHttpSource() {
     override val client = network.cloudflareClient
 
     override fun headersBuilder() = super.headersBuilder()
-        .add("User-Agent", "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36")
+        .add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         .add("Referer", "$baseUrl/")
         .add("Origin", baseUrl)
 
@@ -150,7 +150,7 @@ class FrAnime : AnimeHttpSource() {
             val playerUrl = client.newCall(GET(apiUrl, headers)).await().body.string()
             when (playerName) {
                 "sendvid" -> sendvidExtractor.videosFromUrl(playerUrl)
-                "sibnet" -> sibnetExtractor.videosFromUrl(playerUrl)
+                "sibnet" -> sibnetExtractor.videosFromUrl(playerUrl).map { it.copy(headers = newHeaders) }
                 "vk" -> vkExtractor.videosFromUrl(playerUrl)
                 "vidmoly" -> vidMolyExtractor.videosFromUrl(playerUrl)
                 else -> emptyList()
